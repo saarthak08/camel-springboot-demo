bb80cd8bb001da7bcfc65e0c0f121d0913fe2fb5
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-01-13 17:22:10 +0530

    chore: add Hello Service

---
+++ hello-service/src/main/java/com/sg/helloservice/HelloController.java
@@ -0,0 +1,14 @@
+package com.sg.helloservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello World!";
+    }
+
+}
